71b8e56b6bed2719c4168cf56ab5ba7f6144adf0
Amit Shah <amitshah0t7@gmail.com>
2023-05-04 01:47:35 -0500

    initialize zerodev and define halo signer for signer v5.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,50 @@
-import React, { useEffect,} from 'react';
+import React, { useEffect, useState,} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import readKeys from './utils/halo-reader';
+import readKeys, { HaloSigner } from './utils/halo-reader';
+import { getPrivateKeyOwner, getZeroDevSigner, ZeroDevSigner } from '@zerodevapp/sdk'
+import { Signer } from 'ethers';
+
 
 function App() {
-  const onPressHandler = async ()=>{
-    alert("pressed");
-    await readKeys();
+  const [haloSigner, setHaloSigner] = useState<ZeroDevSigner|null>(null);
+  const [address, setAddress] = useState('')
+  const [loading, setLoading] = useState(false)
+  
+  const initializeHaloHandler = async ()=>{
+    setLoading(true)
+    const keys = await HaloSigner.readKeys();
+    const haloSigner = new HaloSigner(keys[0]);
+    const signer = await getZeroDevSigner({
+      projectId: process.env.ZERODEV_PROJECT_ID as string,
+      owner:  getPrivateKeyOwner(`468f0c80d5336c4a45be71fa19b77e9320dc0abaea4fd018e0c49aca90c1db78`),
+    })
+    setAddress(await signer.getAddress())
+    setLoading(false)
   }
+
+  const createWallet = async () => {
+   
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-      <button type="button" onClick={onPressHandler}>Click Me</button>
+      {(() => {
+              if (haloSigner == null){
+                  return (
+                    <button type="button" onClick={initializeHaloHandler}>Initialize Halo</button>
+
+                  )
+              }              
+              return
+              {address && 
+                <div>
+                  <label>Wallet: {address}</label>
+                </div>
+              }
+            })()}
       </header>
     </div>
   );
--- src/utils/halo-reader.ts
+++ src/utils/halo-reader.ts
@@ -1,29 +1,36 @@
 import { listKeys, ethSignMessage } from 'halo-chip'
-import { ethers, Provider, Signer, TransactionRequest, TypedDataDomain, TypedDataField, VoidSigner } from 'ethers'
+//import {  ethers } from 'ethers'
 import { Deferrable, defineReadOnly, resolveProperties, shallowCopy } from "@ethersproject/properties";
 import { Bytes, BytesLike } from "@ethersproject/bytes";
 import { hashMessage } from '@ethersproject/hash'
+import { Provider, TransactionRequest, TransactionResponse } from '@ethersproject/abstract-provider';
+import { Signer, VoidSigner ,TypedDataDomain, TypedDataField} from '@ethersproject/abstract-signer';
 
 export default async function readKeys(){
-    const keys = await listKeys()
-    alert('keys:')
-    keys.map((k:any) => console.log)
-    alert(JSON.stringify(keys));
-    const address = keys[0].address
+    // const keys = await listKeys()
+    // alert('keys:')
+    // keys.map((k:any) => console.log)
+    // alert(JSON.stringify(keys));
+    // const address = keys[0].address
 
-    alert("sign with address:"+address);
-    const signature = await ethSignMessage('test123', 1, address)
-    console.log('signature:', signature)
+    // alert("sign with address:"+address);
+    // const signature = await ethSignMessage('test123', 1, address)
+    // console.log('signature:', signature)
 
-    const signerAddress = ethers.recoverAddress(hashMessage("test123"),signature);
-    alert("recovered:"+signerAddress);
-    alert("done");
+    // const signerAddress = ethers.recoverAddress(hashMessage("test123"),signature);
+    // alert("recovered:"+signerAddress);
+    // alert("done");
 }
 
 export class HaloSigner extends VoidSigner implements Signer {
-
+    
     constructor(address: string, provider?: Provider) {
-        super(address,provider);
+        super(address, provider);    
+    }
+
+    static async readKeys():Promise<string[]>{
+        const keys =await listKeys()
+        return keys;
     }
 
     getAddress(): Promise<string> {
